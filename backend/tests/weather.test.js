08e067439f5e79f64f2cf3b26420922c2ef09ef9
Daniel Suh <23denial@gmail.com>
2024-06-05 11:10:42 -0400

    weather at point is working

--- backend/tests/weather.test.js
+++ backend/tests/weather.test.js
@@ -1,23 +1,10 @@
-const { describe, expect, test } = require("@jest/globals");
-const { MILLISECOND_EPSILON } = require("../utils/test_utils");
-const { getDiffHourly, getHourlyJSONTime } = require("../utils/weather");
-const dayjs = require("dayjs");
+const { describe, test, expect } = require("@jest/globals");
+const { getWeatherAtPointTime } = require("../utils/weather");
+const dayjs = require("dayjs")
 
-describe("ensure api's hourly value is correct", () => {
-  test("today + 0 hours", () => {
-    const diff = getDiffHourly(dayjs());
-    
-    expect(diff).toBeCloseTo(0, MILLISECOND_EPSILON);
+describe("getWeatherAtPointTime", () => {
+  test("", async () => {
+    let weather = await getWeatherAtPointTime(41.4995, -81.6954, dayjs().format("YYYY-MM-DDTHH:[00]"));
+    expect(weather).toBeDefined();
   });
-  
-  test("today + 12 hours", () => {
-    const date = dayjs(new Date()).add(12, 'hours');
-    const diff = getDiffHourly(date);
-    
-    expect(diff).toBeCloseTo(12 * 60 * 60 * 1000, MILLISECOND_EPSILON);
-  });
-
-  test("getHourlyJSON current time", () => {
-    getHourlyJSONTime(dayjs().unix());
-  })
 });
--- backend/utils/weather.js
+++ backend/utils/weather.js
@@ -5,43 +5,56 @@ const timezone = require("dayjs/plugin/timezone");
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-let getDiffHourly = (time) => {
-  const currentDate = dayjs(),
-    futureDate = dayjs(time);
-  const diff = futureDate.diff(currentDate);
-
-  return diff;
-};
-
-/*
-take the time the user inputs
-convert that time (dayjs) into gmt
-subtract the first gmt time on open-meteo from the inputted time
-convert that to hours
-*/
+const baseURL =
+  "https://api.open-meteo.com/v1/forecast?&hourly=temperature_2m,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,cloud_cover,visibility,wind_speed_10m,wind_gusts_10m,uv_index,is_day&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=auto&forecast_days=1";
 
 /**
  *
- * @param {number} time The unix time for which we should find the `hourly.time` number for.
- * @returns {number} The hour in floating-point form for which we should obtain the weather for.
+ * @param {number} latitude latitude to get weather for
+ * @param {number} longitude longitude to get weather for
+ * @param {dayjs.Dayjs} time time LOCALIZED to the specific (longitude, latitude)
+ * @returns {object} the data object with the respective data
  */
-let getHourlyJSONTime = (time) => {
-  const timeGMT = dayjs.tz(time * 1000, "GMT");
-  console.log(timeGMT.format("YYYY-MM-DDTHH:mm:ss Z"));
+let getWeatherAtPointTime = async (latitude, longitude, time) => {
+  const url = `${baseURL}&latitude=${latitude}&longitude=${longitude}`;
 
-  // dayjs.startOf does not work and is an unmaintained package, so implement it ourselves
-  const startOfDayGMT = timeGMT.hour(0).minute(0).second(0).millisecond(0);
-  console.log(startOfDayGMT.format("YYYY-MM-DDTHH:mm:ss Z"));
+  const weather = await fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("error getting data from weather API.");
+    })
+    .then((responseJSON) => {
+      const timeID = responseJSON.hourly.time.findIndex((t) => t === time);
 
-  return timeGMT.diff(startOfDayGMT, 'h', true)
-};
+      const weatherObject = {
+        time: responseJSON.hourly.time[timeID],
+        temperature_2m: responseJSON.hourly.temperature_2m[timeID],
+        precipitation_probability: responseJSON.hourly.precipitation_probability[timeID],
+        precipitation: responseJSON.hourly.precipitation[timeID],
+        rain: responseJSON.hourly.rain[timeID],
+        showers: responseJSON.hourly.showers[timeID],
+        snowfall: responseJSON.hourly.snowfall[timeID],
+        snow_depth: responseJSON.hourly.snow_depth[timeID],
+        cloud_cover: responseJSON.hourly.cloud_cover[timeID],
+        visibility: responseJSON.hourly.visibility[timeID],
+        wind_speed_10m: responseJSON.hourly.wind_speed_10m[timeID],
+        wind_gusts_10m: responseJSON.hourly.wind_gusts_10m[timeID],
+        uv_index: responseJSON.hourly.uv_index[timeID],
+        is_day: responseJSON.hourly.is_day[timeID]
+      };
 
-let getWeatherAtPointTime = ({ latitude, longitude }, time) => {
-  console.log(latitude, longitude, time);
+      return weatherObject;
+    })
+    .catch((error) => {
+      console.log(error);
+      return;
+    });
+
+    return weather;
 };
 
 module.exports = {
   getWeatherAtPointTime,
-  getDiffHourly,
-  getHourlyJSONTime,
 };
