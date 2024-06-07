9a37adc407cc0dba9d394448f7b66d77d2275e31
Daniel Suh <23denial@gmail.com>
2024-06-07 11:45:57 -0400

    render map

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import { useState } from 'react'
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl";
 
 function App() {
+  useEffect(() => {
+    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
   return (
-    <h1>
-      Hello World
-    </h1>
-  )
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
 }
 
-export default App
+export default App;
